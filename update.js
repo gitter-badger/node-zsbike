473876535c61e84003723bd1923bb47e94344478
ZhengWeiDong <sarices@gmail.com>
2014-10-30 23:51:07 +0800

    修改更新程序保存的mongodb

--- update.js
+++ update.js
@@ -2,13 +2,15 @@
 * @Author: weidong
 * @Date:   2014-10-29 21:04:19
 * @Last Modified by:   weidong
-* @Last Modified time: 2014-10-29 23:58:59
+* @Last Modified time: 2014-10-30 23:55:23
 */
 var cfg = require('./config.json');
 var moment = require('moment');
 var crypto =  require('crypto');
 var url = cfg.bike_api_url;
 var request = require('request');
+var models = require('./model');
+var Station = models.Station;
 console.log('开始时间',moment().format());
 var req = request(url,function(error,response,body){
 	if(!error&&response.statusCode==200){
@@ -17,11 +19,8 @@ var req = request(url,function(error,response,body){
 			console.log('成功获取数据并解析',moment().format());
 			var station = data.station;
 			for(d in data.station){
-				uploadToAvos(station[d]);
-			}
-			//for(d in data.station){
-			//	uploadToAvos(data[d]);
-			//}						
+				saveToDb(station[d]);
+			}					
 		}catch(e){
 			console.error('发生错误'+e);
 		}
@@ -33,80 +32,40 @@ var req = request(url,function(error,response,body){
 	}
 });
 
-function uploadToAvos(data){
+/**
+ * 根据ID获取站点信息
+ * @param  {[type]}   id       [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function getStationById(id, callback) {
+	Station.findOne({'sid': id}, callback);
+};
+
+function saveToDb(data){
 	console.log('开始更新站点',data.id,' ',data.name);
-	data.sid = data.id;
-	delete(data.id);
-	var options = buildOptions();
-	options.params = 'sid='+data.id;
-	var req2 = request(options,function(error,response,body){
-		if (!error) {
-			if(response.statusCode == 200){
-	        	var info = JSON.parse(body);
-	        	if(info.results.length>0){
-		        	info = info.results[0];
-		        	console.log('站点',data.name,'已经有数据,上次更新时间',info.updatedAt,'，需要更新到avoscloud');
-		        	var options = buildOptions();
-		        	options.url += '/'+info.objectId;
-		        	options.method = "PUT";
-		        	options.body = JSON.stringify(data);
-		        	request(options,function(error,response,body){
-		        		if(!error&&response.statusCode==200){
-		        			console.log('站点',data.name,'更新成功！');
-		        		}
-		        		else{
-		        			console.error('站点'+data.name+'更新数据到avos的时候发生错误！');
-							console.log('statuscode'+response.statusCode);
-							console.log(body);
-							console.log('=====================');
-		        		}
-		        	});
-	        	}
-	        	else{
-	        		console.log('站点',data.name,'没有数据,需要上传到到avoscloud');
-		        	var options = buildOptions();
-		        	options.method = "POST";
-		        	options.body = JSON.stringify(data);
-		        	request(options,function(error,response,body){
-		        		if(!error&&response.statusCode==201){
-		        			console.log('站点',data.name,'上传成功！');
-		        		}
-		        		else{
-		        			console.error('站点'+data.name+'上传数据到avos的时候发生错误！');
-							console.log('statuscode'+response.statusCode);
-							console.log(body);
-							console.log('=====================');
-		        		}
-		        	});
-	        	}
-	    	}
-	    }
-	    else{
-	    	console.error('站点'+data.name+'在avoscloud获取数据错误');
-			console.log('statuscode'+response.statusCode);
-			console.log(body);
-			console.log('=====================');
-	    }
-	});
-}
-
-function sign(){
-	var timestamp = moment().unix();
-	var str = timestamp+cfg.avos.key;
-	var sign = crypto.createHash('md5').update(str).digest('hex');
-	return sign+','+timestamp;
-}
-
-function buildOptions(_m){
-
-	var options = {
-		url : cfg.avos.url+'classes/Stations',
-		headers:{
-			'User-Agent': 'node-zsbike',
-			'Content-Type': 'application/json',
-			'X-AVOSCloud-Application-Id': cfg.avos.id,
-			'X-AVOSCloud-Request-Sign': sign()
+	getStationById(data.id,function(err,station){
+		if(err){
+			console.error(err);
 		}
-	};
-	return options;
+		else{
+			console.log(station);
+			if(station==null){
+				console.log('站点',data.name,'没有数据,需要新增记录到数据库');
+			 	station = new Station();
+			}
+			else{
+				console.log('站点',data.name,'已经有数据,上次更新时间',station.updated_at,'，更新数据库记录');
+			}
+			station.name = data.name;
+			station.address = data.address;
+			station.lat = data.lat;
+			station.lng = data.lng;
+			station.capacity = data.capacity;
+			station.availbike = data.availBike;
+			station.sid = data.id;
+			station.save();
+			console.log('站点',data.name,'更新成功！');
+		}
+	});
 }
